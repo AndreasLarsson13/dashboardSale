fa41c37c11a53a8225372e5b8a6bf44a63b90392
Andreas Larsson <larssonandreas316@gmail.com>
2025-07-01 23:29:22 +0300

    update name

--- src/components/VariationItem.js
+++ src/components/VariationItem.js
@@ -58,7 +58,7 @@ const ProductItem = ({ product, onDelete }) => {
     >
       <div style={{ display: "flex", alignItems: "center", justifyContent: "space-between" }}>
         <h2 style={{ color: "#333", marginBottom: "10px" }}>
-          {product.name} - {product.status === "confirmed" 
+         {product.name_parrent} - {product.name} - {product.status === "confirmed" 
             ? "Godkänd" 
             : product.status === "rejected" 
             ? "Avvisad" 
