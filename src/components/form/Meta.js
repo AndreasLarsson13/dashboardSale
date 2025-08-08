e47cae467af2b6c9501690dbecc6bbd5a68331c6
Andreas Larsson <larssonandreas316@gmail.com>
2025-08-08 16:02:12 +0300

    update text

--- src/components/form/Meta.js
+++ src/components/form/Meta.js
@@ -50,8 +50,8 @@ const Meta = ({ product, setProduct, mode = 'edit' }) => {
 { key: 'varmeeffekt', value: { se: 'Värmeeffekt', en: 'Heating power', fi: 'Lämmitysteho' } },
         { key: 'custom', value: { se: 'Anpassad', en: 'Custom', fi: 'Mukautettu' } },
 { key: 'bastuvolymkbm', value: { se: 'Bastuvolym (m³)', en: 'Sauna volume (m³)', fi: 'Saunan tilavuus (m³)' } },
-{ key: 'stenmassakg', value: { se: 'Stenmassa (MAX) (kg)', en: 'Stone mass (MAX) (kg)', fi: 'Kivimassa (MAX) (kg)' } },
-{ key: 'diameter', value: { se: 'Diameter (cm)', en: 'Diameter (cm)', fi: 'Halkaisija (cm)' } },
+{ key: 'stenmassakg', value: { se: 'Stenmassa (MAX) (kg)', en: 'Stone mass (MAX) (kg)', fi: 'Kivimassa (MAKS) (kg)' } },
+{ key: 'diameter', value: { se: 'Diameter', en: 'Diameter', fi: 'Halkaisija' } },
 
     ], []);
 
